62d38da1719c221775ef44a1e737459a7ea50304
Rahul Kumar Singh <37482171+rahulkumarsingh73690@users.noreply.github.com>
2020-11-02 13:35:13 +0530

    Removed unecessary code

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,20 +8,16 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -36,28 +32,46 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/firebase',
   ],
+
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyCG49vI6nrKb7KSL8CTHVdXwY5YXUwTrl8',
+      authDomain: 'quiz-68c4e.firebaseapp.com',
+      databaseURL: 'https://quiz-68c4e.firebaseio.com',
+      projectId: 'quiz-68c4e',
+      storageBucket: 'quiz-68c4e.appspot.com',
+      messagingSenderId: '406134368861',
+      appId: '1:406134368861:web:0440a01d7bbb0a668dfbb4',
+      measurementId: 'G-EXMMDQSYFT',
+    },
+    services: {
+      auth: true, // Just as example. Can be any other service.
+    },
+  },
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
+        light: {
+          primary: colors.blue.darken3, // #E53935
+          secondary: colors.blue.darken2, // #FFCDD2
+          accent: colors.indigo.base, // #3F51B5
+          button: '#fff',
+        },
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          //here you will define primary secondary stuff for dark theme
+          primary: '#000000',
+          secondary: colors.grey.darken4,
+        },
+      },
+      light: true,
+    },
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {},
 }
